8a5bae3b36fba45189d569dabefff53345417988
Ada Void <oxiggy@oxiggy.com>
2024-08-14 17:35:54 +0400

    dev: github workflow

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
+const BASE_PATH = process.env.BASE_PATH
+
 export default defineConfig({
+  base: BASE_PATH ? `${BASE_PATH}/` : '/',
   plugins: [
     remix({
       ssr: false,
       appDirectory: "src",
+      basename: BASE_PATH ? `${BASE_PATH}/` : '/',
+      publicPath: BASE_PATH ? `${BASE_PATH}/` : '/',
     }),
     tsconfigPaths(),
     svgr(),
